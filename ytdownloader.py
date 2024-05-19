89fa81190214e3c77ac265a0975e6ca2a994925d
@bitcoincapitalistnetbitcoincapitalistnet <whyytebouyy01@protonmail.com>
2024-05-19 21:21:18 +0100

    update

--- ytdownloader.py
+++ ytdownloader.py
@@ -21,19 +21,22 @@ def download_playlist(url, output_path):
 
 def download_video(url, output_path):
     try:
-        print("Proccessing Video...")
+        print("Processing Video...")
         yt = YouTube(url)
         stream = yt.streams.filter(res="720p", progressive=True, file_extension='mp4').first()
+        
+        if stream is None:
+            print(f"No suitable stream found for {yt.title}")
+            return
+        
         print(f"Downloading {yt.title}...")        
         # Create directory if it doesn't exist
-        if output_path.lower() == "." or " " or "":
-            stream.download(output_path=output_path, filename=yt.title)
-            file_path = f"{yt.title}.mp4"
-            stream.download(filename=yt.title)
-        else:
-            os.makedirs(output_path, exist_ok=True)
-            file_path = f"{output_path}/{yt.title}.mp4"
-            stream.download(output_path=output_path, filename=yt.title)        
+        if not output_path:
+            output_path = "."  # Default to current directory
+        
+        os.makedirs(output_path, exist_ok=True)
+        stream.download(output_path=output_path, filename=yt.title)
+        file_path = os.path.join(output_path, f"{yt.title}.mp4")
            
         print(f"Downloaded: {yt.title} \n")
     except Exception as e:
@@ -41,7 +44,7 @@ def download_video(url, output_path):
 
 if __name__ == "__main__":
     url = input("Enter the YouTube URL: ")
-    output_path = input("Enter the output directory to save the videos: ").title()
+    output_path = input("Enter the output directory to save the videos: ").strip()
     print("Detecting URL Type...\n")
     if 'playlist' in url.lower():
         download_playlist(url, output_path)
